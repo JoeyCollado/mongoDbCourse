9ab2e92a555f2a4c275f2ca2197868448b7854a9
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-07 08:40:46 +0800

    introduction

--- introduction/main.js
+++ introduction/main.js
@@ -1,1 +1,8 @@
-//MongoDB = is a no sql database management system
+//MongoDB = is a no sql database management system that can manage huge data, use a nosql format unlike sql which is relational
+//N = not 
+//O = only
+//S = structured
+//Q = query
+//L = language
+// = data stores in various formats
+// = instead of table we stored data as a single document
