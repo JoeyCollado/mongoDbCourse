8894ff1e154c1ed2cc35efef34334f6e7a4609d2
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-07 08:47:23 +0800

    done introduction

--- introduction/main.js
+++ introduction/main.js
@@ -6,3 +6,12 @@
 //L = language
 // = data stores in various formats
 // = instead of table we stored data as a single document
+// = each document is kind of like a single row in a table in sql
+// = data is stored as field value pairs, similar to json format, but technically it's bsawn binary javascript object notation
+// = both behaves similarly
+
+// = general idea is = data which is frequently accessed together is stored together rather than in separate tables
+
+//document = is a field of group value pairs to represent an object
+//collection = is a group of one or more documents 
+//database = is a group of one or more collections 
