98d1966a5758999877e881a182aace60bdce2ae0
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-23 18:17:44 +0800

    insert db

---
+++ createDb/main.js
@@ -0,0 +1,18 @@
+//mongosh = establish connection
+//showdbs = show current database
+//cls = clear screen
+//use(dbname) = to use a database
+//db.createCollection("collectionName") = to create a collection in database
+//db.dropDatabase() // to delete a database
+
+//you can automatically create a database by using (use)
+//example = use school (if db name is not been created yet)
+
+//how to insert document in db
+//db.students.insertOne({name: "Spongebob", age:17, gpa:3.14}) = example
+
+//return documents of db
+//db.students.find() = example
+
+//how to insert many documents at once
+//db.students.insertMany([{name:"patrick", age:38, gpa:1.5}, {name:"sandy", age:21, gpa:4.0}, {name:"squid", age:22, gpa:4.4}])  = example
